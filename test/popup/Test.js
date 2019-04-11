fa705f4ae1de9dff55f02b510a7cc5f0ebb4c5c0
twipixel <twipixel@gmail.com>
2019-04-11 22:28:36 +0900

    popup 생성

---
+++ test/popup/Test.js
@@ -0,0 +1,34 @@
+
+
+const LINE_PATH = [
+];
+
+
+export default class Test extends PIXI.Container {
+    constructor(renderer) {
+        super();
+
+        this.interactive = true;
+        this.renderer = renderer;
+        this.canvas = this.renderer.view;
+        this.context = this.canvas.getContext('2d');
+
+        this.initialize();
+        this.addEvent();
+    }
+
+    initialize() {
+
+    }
+
+    addEvent() {
+    }
+
+    update() {
+    }
+
+    resize() {
+        this.hitArea = new PIXI.Rectangle(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+}
