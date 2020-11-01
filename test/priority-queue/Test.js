f5589c5a1df0bbbbc6fd49a290c07760b70ccaac
choi.sungryeol <choi.sungryeol@navercorp.com>
2020-11-01 15:58:07 +0900

    Priority Queue 샘플 코드 추가

--- test/priority-queue/Test.js
+++ test/priority-queue/Test.js
@@ -12,5 +12,24 @@ export default class Test {
         const head = prq.poll();
         console.log('head', head);
 
+        const prq1 = new PriorityQueue();
+        prq1.add(1);
+        prq1.add(0);
+        prq1.add(5);
+        prq1.add(4);
+        prq1.add(3);
+        prq1.add(1);
+        prq1.add(0);
+        console.log('---------------');
+        console.log('prq1', prq1);
+        // 최우선 순위 반환
+        console.log(prq1.peek());
+        console.log(prq1.size);
+        while(!prq1.isEmpty()) {
+            // 최우선 순위 요소 제거
+            console.log(prq1.poll());
+        }
+        // 사용하지 않는 메모리 복구
+        prq1.trim();
     }
 }
